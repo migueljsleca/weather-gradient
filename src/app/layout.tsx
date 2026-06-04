1c3f6ae212f4aa01e1dbd6c9647381dc188d5c76
migueljsleca <migueljsleca@gmail.com>
2026-06-04 15:55:28 +0100

    Add Cloudflare analytics beacon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token":"ddab161b3aea4307a7d4d4f9c2792347"}'
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
